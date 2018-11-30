a63aee3ee23c247a31f819a4461d4d59a805dd52
Eric Tran <tranbi_e@epita.fr>
2018-11-30 21:58:57 +0100

    getVolatility via API

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,65 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
+	private void getVolatility(List<Integer> ratios, List<Integer> assetsIds, String startDate = "", String endDate = "") {
+		final RatioMultiAssetParamModel locParam = new RatioMultiAssetParamModel(ratios, assetsIds, null, startDate, endDate, null);
+		final String locURI = URL + "ratio/invoke";
+
+		URIBuilder locURIBuilder;
+		HttpPost locHttpPost;
+		String locResponse;
+		ClosableHttpClient client;
+		HttpClientContext clientContext;
+
+		try {
+			locURIBuilder = new URIBuilder(locURI);
+			locHttpPost = RequestUtil.createPost(locURIBuilder, locParam);
+			AuthCache authCache = new BasicAuthCache();
+			BasicScheme basicAuth = new BasicScheme();
+			final HttpHost locLocalhost = new HttpHost("dolphin.jump-technology.com", 3472, "https");
+			authCache.put(locLocalhost, )
+			clientContext = HttpClientContext.create();
+			clientContext.setAuthCache(authCache);
+			CredentialsProvider credsProvider = new BasicCredentialsProvider();
+			credsProvider.setCredentials(
+					new AuthScope(locLocalhost),
+					new UsernamePasswordCredentials("epita_user_3", "dkw3JReNdZmZ6WV4"));
+			SSLContextBuilder builder = new SSLContextBuilder();
+			SSLConnectionSocketFactory sslsf = null;
+			try {
+				builder.loadTrustMaterial(null, new TrustSelfSignedStrategy(){
+					@Override
+					public boolean isTrusted(X509Certificate[] chain,
+											 String authType) throws CertificateException {
+						return true;//FIXME : A MODIFIER QUAND HTTPS OK
+					}
+				});
+				sslsf = new SSLConnectionSocketFactory(
+						builder.build(), NoopHostnameVerifier.INSTANCE);//FIXME : A MODIFIER QUAND HTTPS OK
+				client = HttpClientBuilder.create()
+						.setDefaultCredentialsProvider(credsProvider)
+						.setSSLSocketFactory(sslsf)
+						.build();
+				locResponse = executeRequest(client, clientContext, locHttpPost);
+				System.out.println(locResponse);
+			} catch (NoSuchAlgorithmException | KeyManagementException | KeyStoreException parE) {
+				parE.printStackTrace();
+			}
+		} catch (URISyntaxException locE) {
+			System.out.println("Vérifier la syntaxe de la requete "+locURI);
+			return null;
+		}
+		if (locResponse == null) {
+			return null;
+		}
+	}
+	public static void main(String[] args) {
+		List<Integer> ratios = new ArrayList<>();
+		ratios.add(18);
+		List<Integer> assetsIds = new ArrayList<>();
+		assetsIds.add(599);
+		getVolatility(ratios, assetsIds);
+		/*
 		JsonReader reader = null;
 		InputStream istream = null;
 		try {
@@ -39,6 +97,6 @@ public class Main {
 
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
-		}
+		}*/
 	}
 }
